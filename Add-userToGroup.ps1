564d0517873d92142184ded9cedee217890ece40
Rod Stewart <rodstewart@gmail.com>
2017-10-30 12:01:53 -0500

    Init Commit

---
+++ Add-userToGroup.ps1
@@ -0,0 +1,33 @@
+<#
+.SYNOPSIS
+  Short description
+.DESCRIPTION
+  Long Description
+.EXAMPLE
+  PS C:\> .\Add-userToGroup.ps1
+  Explanation of what the example does
+.INPUTS
+  Inputs (if any)
+.OUTPUTS
+  Output (if any)
+.NOTES
+  Written by Rod Stewart
+#>
+
+[cmdletbinding()]
+param(
+  [Parameter(ValueFromPipeline)]
+  $ParamOne
+)
+
+Begin {
+
+}
+
+Process {
+
+}
+
+End {
+
+}
